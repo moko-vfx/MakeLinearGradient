c1c82013c1df16d54db2678c5d85ac2ac537ffcd
moko-vfx <moko.vfx@gmail.com>
2018-05-06 21:18:38 +0900

    MakeLinearGradient_v1をアップロード
    ソリューション一式と実行ファイル

---
+++ source/MyTool_MakeLinearGradient/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace MyTool_MakeLinearGradient
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox_g1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox_g2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox_g3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox_g4 = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_g1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_g2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_g3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_g4)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("メイリオ", 10F);
+			this.label1.Location = new System.Drawing.Point(17, 10);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(290, 21);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "▼クリックでグラデーション画像を作成する";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button1.Location = new System.Drawing.Point(178, 34);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(53, 32);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "保存";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(208)))), ((int)(((byte)(92)))));
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Location = new System.Drawing.Point(34, 76);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox1.TabIndex = 11;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox_g1
+			// 
+			this.pictureBox_g1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox_g1.Location = new System.Drawing.Point(34, 35);
+			this.pictureBox_g1.Name = "pictureBox_g1";
+			this.pictureBox_g1.Size = new System.Drawing.Size(30, 30);
+			this.pictureBox_g1.TabIndex = 12;
+			this.pictureBox_g1.TabStop = false;
+			this.pictureBox_g1.Click += new System.EventHandler(this.pictureBox_g1_Click);
+			// 
+			// pictureBox_g2
+			// 
+			this.pictureBox_g2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox_g2.Location = new System.Drawing.Point(70, 35);
+			this.pictureBox_g2.Name = "pictureBox_g2";
+			this.pictureBox_g2.Size = new System.Drawing.Size(30, 30);
+			this.pictureBox_g2.TabIndex = 12;
+			this.pictureBox_g2.TabStop = false;
+			this.pictureBox_g2.Click += new System.EventHandler(this.pictureBox_g2_Click);
+			// 
+			// pictureBox_g3
+			// 
+			this.pictureBox_g3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox_g3.Location = new System.Drawing.Point(106, 35);
+			this.pictureBox_g3.Name = "pictureBox_g3";
+			this.pictureBox_g3.Size = new System.Drawing.Size(30, 30);
+			this.pictureBox_g3.TabIndex = 12;
+			this.pictureBox_g3.TabStop = false;
+			this.pictureBox_g3.Click += new System.EventHandler(this.pictureBox_g3_Click);
+			// 
+			// pictureBox_g4
+			// 
+			this.pictureBox_g4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox_g4.Location = new System.Drawing.Point(142, 35);
+			this.pictureBox_g4.Name = "pictureBox_g4";
+			this.pictureBox_g4.Size = new System.Drawing.Size(30, 30);
+			this.pictureBox_g4.TabIndex = 12;
+			this.pictureBox_g4.TabStop = false;
+			this.pictureBox_g4.Click += new System.EventHandler(this.pictureBox_g4_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel1.Location = new System.Drawing.Point(0, 342);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(324, 16);
+			this.panel1.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button2.Location = new System.Drawing.Point(237, 35);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(53, 32);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Help";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(324, 358);
+			this.Controls.Add(this.pictureBox_g4);
+			this.Controls.Add(this.pictureBox_g2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.pictureBox_g3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox_g1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Form1";
+			this.Text = "Make Linear Gradient 256";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_g1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_g2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_g3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_g4)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox_g1;
+		private System.Windows.Forms.PictureBox pictureBox_g2;
+		private System.Windows.Forms.PictureBox pictureBox_g3;
+		private System.Windows.Forms.PictureBox pictureBox_g4;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ source/MyTool_MakeLinearGradient/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyTool_MakeLinearGradient
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		//4つのグラデーションサンプルを作成
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			//描画先とするImageオブジェクトを作成する
+			Bitmap canvas1 = new Bitmap(pictureBox_g1.Width, pictureBox_g1.Height);
+			//ImageオブジェクトのGraphicsオブジェクトを作成する
+			using (Graphics g = Graphics.FromImage(canvas1))
+			{
+				//縦に白から黒へのグラデーションのブラシを作成
+				using (LinearGradientBrush gb = new LinearGradientBrush(
+							g.VisibleClipBounds,
+							Color.White,
+							Color.Black,
+							LinearGradientMode.Vertical))
+				{
+					//四角を描く
+					g.FillRectangle(gb, g.VisibleClipBounds);
+
+					//PictureBox1に表示する
+					pictureBox_g1.Image = canvas1;
+				}
+			}
+
+			//描画先とするImageオブジェクトを作成する
+			Bitmap canvas2 = new Bitmap(pictureBox_g1.Width, pictureBox_g1.Height);
+			//ImageオブジェクトのGraphicsオブジェクトを作成する
+			using (Graphics g = Graphics.FromImage(canvas2))
+			{
+				//縦に黒から白へのグラデーションのブラシを作成
+				using (LinearGradientBrush gb = new LinearGradientBrush(
+							g.VisibleClipBounds,
+							Color.Black,
+							Color.White,
+							LinearGradientMode.Vertical))
+				{
+					//四角を描く
+					g.FillRectangle(gb, g.VisibleClipBounds);
+
+					//PictureBox1に表示する
+					pictureBox_g2.Image = canvas2;
+				}
+			}
+
+			//描画先とするImageオブジェクトを作成する
+			Bitmap canvas3 = new Bitmap(pictureBox_g1.Width, pictureBox_g1.Height);
+			//ImageオブジェクトのGraphicsオブジェクトを作成する
+			using (Graphics g = Graphics.FromImage(canvas3))
+			{
+				//横に白から黒へのグラデーションのブラシを作成
+				using (LinearGradientBrush gb = new LinearGradientBrush(
+							g.VisibleClipBounds,
+							Color.White,
+							Color.Black,
+							LinearGradientMode.Horizontal))
+				{
+					//四角を描く
+					g.FillRectangle(gb, g.VisibleClipBounds);
+
+					//PictureBox1に表示する
+					pictureBox_g3.Image = canvas3;
+				}
+			}
+
+			//描画先とするImageオブジェクトを作成する
+			Bitmap canvas4 = new Bitmap(pictureBox_g1.Width, pictureBox_g1.Height);
+			//ImageオブジェクトのGraphicsオブジェクトを作成する
+			using (Graphics g = Graphics.FromImage(canvas4))
+			{
+				//横に黒から白へのグラデーションのブラシを作成
+				using (LinearGradientBrush gb = new LinearGradientBrush(
+							g.VisibleClipBounds,
+							Color.Black,
+							Color.White,
+							LinearGradientMode.Horizontal))
+				{
+					//四角を描く
+					g.FillRectangle(gb, g.VisibleClipBounds);
+
+					//PictureBox1に表示する
+					pictureBox_g4.Image = canvas4;
+				}
+			}
+		}
+
+		//256x256サイズのImageオブジェクトを作成
+		Bitmap img = new Bitmap(256, 256);
+
+		private void pictureBox_g1_Click(object sender, EventArgs e)
+		{
+			//ImageオブジェクトのGraphicsオブジェクトを作成
+			using (Graphics g = Graphics.FromImage(img))
+			{
+				for (int i = 0; i < 256; i++)
+				{
+					// 色は黒で幅1pxのペン
+					using (Pen p = new Pen(Color.FromArgb(255, i, i, i), 1))
+					{
+						//グラデーションを描画
+						g.DrawLine(p, 0, i, 255, i);
+					}
+				}
+
+				//作成した画像を表示する
+				pictureBox1.Image = img;
+			}
+		}
+
+		private void pictureBox_g2_Click(object sender, EventArgs e)
+		{
+			//ImageオブジェクトのGraphicsオブジェクトを作成
+			using (Graphics g = Graphics.FromImage(img))
+			{
+				for (int i = 0; i < 256; i++)
+				{
+					// 色は黒で幅1pxのペン
+					using (Pen p = new Pen(Color.FromArgb(255, i, i, i), 1))
+					{
+						//グラデーションを描画
+						g.DrawLine(p, 0, 255 - i, 255, 255 - i);
+					}
+				}
+
+				//作成した画像を表示する
+				pictureBox1.Image = img;
+			}
+		}
+
+		private void pictureBox_g3_Click(object sender, EventArgs e)
+		{
+			//ImageオブジェクトのGraphicsオブジェクトを作成
+			using (Graphics g = Graphics.FromImage(img))
+			{
+				for (int i = 0; i < 256; i++)
+				{
+					// 色は黒で幅1pxのペン
+					using (Pen p = new Pen(Color.FromArgb(255, i, i, i), 1))
+					{
+						//グラデーションを描画
+						g.DrawLine(p, i, 0, i, 255);
+					}
+				}
+
+				//作成した画像を表示する
+				pictureBox1.Image = img;
+			}
+		}
+
+		private void pictureBox_g4_Click(object sender, EventArgs e)
+		{
+			//ImageオブジェクトのGraphicsオブジェクトを作成
+			using (Graphics g = Graphics.FromImage(img))
+			{
+				for (int i = 0; i < 256; i++)
+				{
+					// 色は黒で幅1pxのペン
+					using (Pen p = new Pen(Color.FromArgb(255, i, i, i), 1))
+					{
+						//グラデーションを描画
+						g.DrawLine(p, 255 - i, 0, 255 - i, 255);
+					}
+				}
+
+				//作成した画像を表示する
+				pictureBox1.Image = img;
+			}
+		}
+
+		//グラデーション画像を作成して保存する
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//PixtureBoxが空じゃなければ実行
+			if (pictureBox1 != null)
+			{
+				//作成した画像をデスクトップに保存
+
+				// フィルターの設定
+				saveFileDialog1.Filter = "BMP形式|*.bmp";
+				// ファイル保存ダイアログを表示
+				saveFileDialog1.ShowDialog();
+				// フォーマット指定
+				img.Save(saveFileDialog1.FileName, ImageFormat.Bmp);
+			}
+			else
+			{
+				MessageBox.Show("保存する前にグラデーションを作成してください");
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show(
+				"1 ピクセルごとに色の値が 1 ずつ増えていく\r\n" +
+				"256ｘ256 の綺麗なリニアグラデーションを作成します"
+				);
+		}
+	}
+
+}
